649a9fce5196e3f1730a114c27ca225001a43d7d
Ran Mao <28978705+MrRanMao@users.noreply.github.com>
2023-04-11 16:36:21 +0300

    Insert DB Template

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ chmod +x /root/shell/*
 echo $1 > /root/shell/config
 
 cp ./src/etc/billmgr_mod_service_vds-aeza.xml /usr/local/mgr5/etc/xml/
+mysql -u root -p billmgr < ./src/etc/db_with_products.sql
 
 /usr/local/mgr5/sbin/mgrctl -m billmgr --restart
