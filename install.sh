6a8e69170fed6d0aee9ac49807ffc713f3df4453
Ran Mao <28978705+MrRanMao@users.noreply.github.com>
2023-04-11 12:46:52 +0300

    Add XML and restart mgrctl

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ mkdir -p /root/shell
 cp -r ./src/shell/* /root/shell
 chmod +x /root/shell/*
 echo $1 > /root/shell/config
+
+cp ./src/etc/billmgr_mod_service_vds-aeza.xml /usr/local/mgr5/etc/xml/
+
+/usr/local/mgr5/sbin/mgrctl -m billmgr --restart
